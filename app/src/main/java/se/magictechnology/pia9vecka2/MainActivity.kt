e0c32d6478895e6543f31af77aef88bcaa1884c1
Bill Mårtensson <bill@magictechnology.se>
2021-01-11 13:04:04 +0100

    Other activity

--- app/src/main/java/se/magictechnology/pia9vecka2/MainActivity.kt
+++ app/src/main/java/se/magictechnology/pia9vecka2/MainActivity.kt
@@ -1,8 +1,11 @@
 package se.magictechnology.pia9vecka2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -43,12 +46,72 @@ class MainActivity : AppCompatActivity() {
         var resetButton = findViewById<Button>(R.id.resetButton)
 
         resetButton.setOnClickListener {
-            counterNumber = 0
+            resetCounting()
+        }
+
+
+        var setNumberButton = findViewById<Button>(R.id.setNumberButton)
+
+        setNumberButton.setOnClickListener {
+
+            var numberEditText = findViewById<EditText>(R.id.numberEditText)
+
+            var enteredText = numberEditText.text.toString()
+
+            Log.d("PIA9DEBUG", enteredText)
+
+            /*
+            var enteredNumber = enteredText.toIntOrNull()
+
+            if(enteredNumber != null)
+            {
+                counterNumber = enteredNumber
+            }
+            */
+
+            /*
+            // Motsvarande i Swift:
+            if let thenumber = enteredText
+            {
+                thenumber
+            }
+             */
+
+            enteredText.toIntOrNull()?.let { enteredNumber ->
+                counterNumber = enteredNumber
+            }
 
             counterTextView.text = counterNumber.toString()
+
+
+            numberEditText.setText("")
+
+        }
+
+
+        var goOtherButton = findViewById<Button>(R.id.goOtherButton)
+
+        goOtherButton.setOnClickListener {
+            Log.d("PIA9DEBUG", "Lets go")
+
+            var intent = Intent(this, OtherActivity::class.java)
+            intent.putExtra("counter", counterNumber)
+            startActivity(intent)
         }
 
     }
+
+
+    fun resetCounting()
+    {
+        counterNumber = 0
+
+        var counterTextView = findViewById<TextView>(R.id.counterTextView)
+
+        counterTextView.text = counterNumber.toString()
+    }
+
+
 
 
 
